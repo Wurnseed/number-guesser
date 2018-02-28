ac1ee96b9b4f26e345b365552b4c1534118699eb
Wurnseed <266okruk@gmail.com>
2018-02-27 22:24:08 -0800

    test
    test

--- preload.js
+++ preload.js
@@ -9,7 +9,7 @@ var wd = wei * size;
 var hd = hei * size;
 
 var img = new Image();
-img.src = 'test.jpg';
+img.src = 'http://image.ibb.co/jfcYnc/1.jpg';
 
 // draw the original image at a fraction of the final size
 ctx.drawImage(img, 0, 0, wd, hd);
@@ -23,14 +23,17 @@ ctx.imageSmoothingEnabled = false;
 // enlarge the minimized image to full size
 ctx.drawImage(canvas, 0, 0, wd, hd, 0, 0, wei, hei);
 //
-for(let i = 0; i < h; i++){
+for(let i = 0; i < h; i++){ 
   for(let j = 0; j < w; i++){
-    var p = ctx.getImageData(j*wd, i*hd, 1, 1).data;
-    if(p[0]+p[1]+p[2]/3 <= 100){
-      pic.push(1);
-    }else{
-      pic.push(0);
-    }
+    var p = ctx.getImageData(j, i, 1, 1).data;
+    console.log(w);
+    console.log(p);
+    break;
+    // if(p[0]+p[1]+p[2]/3 <= 100){
+    //   pic.push(1);
+    // }else{
+    //   pic.push(0);
+    // }
   }
 }
 console.log(pic);
